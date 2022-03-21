84fb76dadc00bec7a663f635039a97f2365ddaa2
Isa Fontana <isfon8@gmail.com>
2022-03-21 14:12:26 -0600

    progress

---
+++ wordplayApp/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  wordplayApp
+//
+//  Created by isa fontana on 3/21/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var sentenceLabel: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- wordplayApp/ViewController.swift
+++ wordplayApp/ViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterWordTextField: UITextField!
+    
+    
+    @IBAction func onEnterButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "mySegue", sender: nil)
+        
+       
+        
+      //  enterWordTextField.resignFirstResponder()
+    }
+    
+override func
+    prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let nvc = segue.destination as! SecondViewController
+      //  sentenceLabel.text = "My uncle wants to go to the \(enterWordTextField.text ?? "")"
+    
+}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
+        
     }
 
 
